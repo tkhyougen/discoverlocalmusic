7f9695aa1fef73a95123fbc2ed7e5b4bdcc887b8
tkhyougen <illgalsubstance@yahoo.co.jp>
2020-05-31 08:23:04 +0000

    devise2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
   # csrf
   protect_from_forgery with: :exception
-  
+
 
 
   # 下のdef configure..を実行
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
     #name image commentの操作を許可する
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
