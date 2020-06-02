fa972a3ba2060f584b3613401767cb613b951688
tkhyougen <illgalsubstance@yahoo.co.jp>
2020-06-02 09:22:52 +0000

    tagit db revise

--- db/migrate/20200519160802_devise_create_users.rb
+++ db/migrate/20200519160802_devise_create_users.rb
@@ -9,7 +9,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string "email",              null: false, default: ""
       t.string "image"
       t.text "comment"
-      t.string "encrypted_password" null: false, default: ""
+      t.string "encrypted_password"  null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -56,16 +56,19 @@ ActiveRecord::Schema.define(version: 2020_05_31_084820) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
+    t.string "image"
+    t.text "comment"
     t.string "encrypted_password", default: ""
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
-    t.string "image"
-    t.text "comment"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
