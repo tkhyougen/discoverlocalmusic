34f801749af39cdf73d4c2196e39889722f968a9
tkhyougen <illgalsubstance@yahoo.co.jp>
2020-06-27 17:35:17 +0000

    localartist show分岐

--- app/models/localartist.rb
+++ app/models/localartist.rb
@@ -1,8 +1,6 @@
 class Localartist < ApplicationRecord
   #アソシエーション　ex.localartists.user.name
   belongs_to :user
-
-
   has_many :comments, dependent: :destroy  #localaritstに対するコメントをネスト
 
   acts_as_taggable
