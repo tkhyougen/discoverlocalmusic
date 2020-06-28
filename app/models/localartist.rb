c35d2958d5000bc718d914dabe945a3942fdd20c
tkhyougen <illgalsubstance@yahoo.co.jp>
2020-06-28 15:50:14 +0000

    Fix:spot検索改定

--- app/models/localartist.rb
+++ app/models/localartist.rb
@@ -4,6 +4,7 @@ class Localartist < ApplicationRecord
   has_many :comments, dependent: :destroy  #localaritstに対するコメントをネスト
 
   validates :name, presence: true, length:{ maximum:20 },uniqueness: true
+  before_validation {country.capitalize!}
   validates :country, presence: true, length:{ maximum:20 }
   validates :post_comment, length:{ maximum:255 }
 
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -4,6 +4,7 @@ class Spot < ApplicationRecord
 
 
   validates :name, presence: true, length:{ maximum:20 },uniqueness: true
+  before_validation {country.capitalize!}
   validates :country, presence: true, length:{ maximum:20 }
   validates :post_comment, length:{ maximum:255 }
 
