ea6856c4b2ca92cd97894edf5dc7e31ea67cc46f
tkhyougen <illgalsubstance@yahoo.co.jp>
2020-07-14 16:45:46 +0000

    画像投稿質問2

--- db/migrate/20200705092538_deleteyoutube.rb
+++
@@ -1,5 +0,0 @@
-class Deleteyoutube < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :youtubes
-  end
-end
--- db/migrate/20200711124557_droplabel.rb
+++
@@ -1,6 +0,0 @@
-class Droplabel < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :user_country_labellings
-    drop_table :user_country_labels
-  end
-end
