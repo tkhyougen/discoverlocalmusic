df37d987ddb4f78d24a80ed74615f30bc66e5d8d
tkhyougen <illgalsubstance@yahoo.co.jp>
2020-07-24 08:01:00 +0000

    faker test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+#config/initializers/local.rbの:jaをコメントアウトしてからrails db:seed
+
 5.times do |n|
   name = Faker::Games::Pokemon.name
   email = Faker::Internet.email
@@ -9,4 +11,16 @@
                password: password,
                password_confirmation: password,
                )
+end
+
+20.times do |n|
+  name1 = Faker::Music::RockBand.name
+  country1 = Faker::Address.country
+  post_comment1 = "for test"
+  random = rand(1..5)
+  Localartist.create!(name: name1,
+                      country: country1,
+                      post_comment: post_comment1,
+                      user_id:random,
+                      )
 end
